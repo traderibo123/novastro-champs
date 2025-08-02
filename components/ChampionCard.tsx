f595d6af4fe779a98e363e54588ad8757bfec721
Traderibo123 <161652314+traderibo123@users.noreply.github.com>
2025-08-02 21:44:08 +0300

    Update ChampionCard.tsx

--- components/ChampionCard.tsx
+++ components/ChampionCard.tsx
@@ -29,7 +29,7 @@ export default function ChampionCard({ username, data }: { username: string, dat
           @{username}
         </div>
 
-        {/* Rank + Title (boş veri için "-" yerine boş bırak) */}
+        {/* Rank + Title (boş veri için hiç göstermiyor) */}
         {[
           { time: '7d', top: 260 },
           { time: '30d', top: 335 },
@@ -56,7 +56,7 @@ export default function ChampionCard({ username, data }: { username: string, dat
           className="absolute bottom-[60px] right-[200px] z-20 px-4 py-2 bg-[#1DA1F2] hover:bg-[#1a8cd8] rounded text-white font-semibold shadow text-sm"
           onClick={() => {
             const tweetText = encodeURIComponent(
-              "Check out my Novastro Champion Card! 🏆 #Novastro $XNL @Novastro_xyz @traderibo123"
+              "Check out my Novastro Champion Card! 👑 #Novastro $XNL @Novastro_xyz @traderibo123"
             )
             const tweetUrl = `https://twitter.com/intent/tweet?text=${tweetText}`
             window.open(tweetUrl, '_blank')
@@ -88,7 +88,6 @@ export default function ChampionCard({ username, data }: { username: string, dat
         <div className="absolute bottom-[25px] w-full text-center text-xs text-gray-400 italic">
           Crafted by <a href="https://x.com/traderibo123" className="hover:underline">@traderibo123</a>
         </div>
-
       </div>
     </div>
   )
